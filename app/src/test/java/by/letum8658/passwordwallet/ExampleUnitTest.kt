233811988061412c0dcf6a8dbabc2f2a7ee904e1
AndreyKa986 <letum8658@tut.by>
2019-08-09 11:24:59 +0300

    Was Added a Few Libraries and Permission

--- app/src/test/java/by/letum8658/passwordwallet/ExampleUnitTest.kt
+++ app/src/test/java/by/letum8658/passwordwallet/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package by.letum8658.passwordwallet
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
